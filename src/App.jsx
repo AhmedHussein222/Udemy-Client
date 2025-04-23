8d83a85ec1b7e6ca0476b80205955b5bb5676af4
Heba_Abdelaal <he32ba2@gmail.com>
2025-04-24 00:58:17 +0200

    UiComponentCart

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './Components/Header/Header'
 import { CssBaseline } from '@mui/material';
 import Footer from './Components/Footer/Footer';
+import Cart from './Components/Cart/Cart';
 
 function App() {
   return (
 <CssBaseline>
   <Header/>
+  <Cart/>
   <Footer/>
+
 </CssBaseline>
    
   )
