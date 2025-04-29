5e85541d8e1ea8d37539ae6df63910a07ebf55cf
Heba_Abdelaal <he32ba2@gmail.com>
2025-04-29 22:39:05 +0300

    Signup/loginForusersDn

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Header from './Components/Header/Header'
 import { CssBaseline } from '@mui/material';
 import Footer from './Components/Footer/Footer';
 import Cart from './Components/Cart/Cart';
+import Signup from './Components/SignUp/Signup';
+import Login from './Components/LoginUser/Login';
+import InsSignup from './Components/InstructorSignup/InsSignup';
 
 function App() {
   return (
 <CssBaseline>
   <Header/>
-  <Cart/>
+  {/* <Signup /> */}
+  {/* <Login /> */}
+  <InsSignup />
   <Footer/>
 
 </CssBaseline>
---
+++ src/Components/SignUp/Signup.jsx
@@ -0,0 +1,237 @@
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    FormGroup,
+    Grid,
+    Stack,
+    TextField,
+    Typography,
+  } from '@mui/material';
+  import MailOutlineIcon from '@mui/icons-material/MailOutline';
+  import React from 'react';
+import { grey } from '@mui/material/colors';
+import { useForm } from "react-hook-form";
+
+  
+  function Signup() {
+    const {register,  handleSubmit, formState: { errors }, } = useForm({
+        defaultValues: {
+          fullname: "",
+          email: "",
+        },
+      });
+      
+      const onSubmit = (data) => {
+        console.log("Form data:", data);
+      };
+      
+    
+    return (
+        <Box sx={{ flexGrow: 1, p: { xs: 2, md: 4 } }}>
+        <Grid container spacing={4} alignItems="center" justifyContent={'space-around'}>
+          
+          <Grid item xs={12} lg={6} sx={{ textAlign: 'center' }}>
+            <Box
+              component="img"
+              src="https://frontends.udemycdn.com/components/auth/desktop-illustration-x1.webp"
+              alt="Illustration"
+              sx={{
+                width: '100%',
+                maxWidth: 600,
+                height: 'auto',
+                mx: 'auto'
+              }}
+            />
+          </Grid>
+      
+          <Grid item xs={12} lg={6}>
+           <Box sx={{ maxWidth: 500, mx: 'auto', px: 2 }}>
+      <Typography
+        variant="h4"
+        gutterBottom
+        fontWeight="bold"
+        textAlign="center"
+      >
+        Sign up with email
+      </Typography>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+  <Stack spacing={2}>
+    <TextField
+      fullWidth
+      size="medium"
+      label="Fullname"
+      variant="outlined"
+      {...register("fullname", { required: true, maxLength: 20 })}
+    />
+    {errors.fullname?.type === "required" && (
+   <Typography color='error'>Fullname is required.</Typography>
+    )}
+
+  <TextField
+       fullWidth
+       size="medium"
+       label="Email"
+       variant="outlined"
+       {...register("email", {
+         required: true,
+         pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+       })}
+     />
+     {errors.email?.type === "required" && (
+       <Typography color='error'>Email is required.</Typography>
+     )}
+     {errors.email?.type === "pattern" && (
+       <Typography color='error'>Email is not valid.</Typography>
+     )}
+
+    <FormGroup>
+      <FormControlLabel
+        control={<Checkbox />}
+        label="Send me special offers, personalized recommendations, and learning tips."
+      />
+    </FormGroup>
+
+    <Button
+      type="submit"
+      fullWidth
+      variant="contained"
+      startIcon={<MailOutlineIcon fontSize="small" />}
+      sx={{
+        backgroundColor: '#8000ff',
+        color: '#fff',
+        textTransform: 'none',
+        fontWeight: 'bold',
+        borderRadius: '4px',
+        py: 1.2,
+        mt: 1,
+        '&:hover': { backgroundColor: '#6a1b9a' },
+      }}
+    >
+      Continue with email
+    </Button>
+  </Stack>
+</form>
+
+
+
+      <Typography
+        variant="body1"
+        align="center"
+        sx={{ mt: 4, fontWeight: 'bold' }}
+      >
+        Other sign up options
+      </Typography>
+
+      <Stack
+        direction="row"
+        gap={2}
+        justifyContent="center"
+        mt={2}
+        mb={3}
+      >
+        <Button
+          variant="outlined"
+          sx={{
+            textTransform: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            gap: 1,
+            px: 2,
+            py: 1,
+            borderRadius: '8px',
+            borderColor: '#8000ff',
+          }}
+        >
+          <img
+            src="https://static.cdnlogo.com/logos/g/38/google-icon.svg"
+            alt="Google"
+            style={{ width: 20, height: 20 }}
+          />
+        </Button>
+
+        <Button
+          variant="outlined"
+          sx={{
+            textTransform: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            gap: 1,
+            px: 2,
+            py: 1,
+            borderRadius: '8px',
+            borderColor: '#8000ff',
+          }}
+        >
+          <img
+            src="https://i.pinimg.com/736x/42/75/49/427549f6f22470ff93ca714479d180c2.jpg"
+            alt="Facebook"
+            style={{ width: 20, height: 20 }}
+          />
+        </Button>
+
+        <Button
+          variant="outlined"
+          sx={{
+            textTransform: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            gap: 1,
+            px: 2,
+            py: 1,
+            borderRadius: '8px',
+            borderColor: '#8000ff',
+          }}
+        >
+          <img
+            src="https://pixsector.com/cache/56f2646e/avd5cee2ff5ea9da4d328.png"
+            alt="Apple"
+            style={{ width: 20, height: 20 }}
+          />
+        </Button>
+      </Stack>
+
+   
+      <Typography
+        variant="subtitle2"
+        align="center"
+        sx={{ mb: 3 }}
+      >
+        By signing up, you agree to our{' '}
+        <a href="#" style={{ color: '#8000ff' }}>
+          Terms of Use
+        </a>{' '}
+        and{' '}
+        <a href="#" style={{ color: '#8000ff' }}>
+          Privacy Policy
+        </a>
+        .
+      </Typography>
+
+      <Box
+        sx={{
+          p: 2,
+          backgroundColor: grey[100],
+          textAlign: 'center',
+          borderRadius: 1,
+        }}
+      >
+        <Typography variant="body2">
+          Already have an account?{' '}
+          <a href="#" style={{ color: '#8000ff', fontWeight: 'bold' }}>
+            Login
+          </a>
+        </Typography>
+      </Box>
+    </Box>
+          </Grid>
+        </Grid>
+      </Box>
+      
+    );
+  }
+  
+  export default Signup;
+  
