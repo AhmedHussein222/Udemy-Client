505670d229fd234293cee11956285525a91c7ad8
Heba_Abdelaal <he32ba2@gmail.com>
2025-05-01 02:42:52 +0300

    InsWelcomePagetosignupDn

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import Header from './Components/Header/Header'
 import { CssBaseline } from '@mui/material';
 import Footer from './Components/Footer/Footer';
 import Cart from './Components/Cart/Cart';
-import Signup from './Components/SignUp/Signup';
-import Login from './Components/LoginUser/Login';
+import Signup from './Components/SignUpStudents/Signup';
+import Login from './Components/LoginUsers/Login';
 import InsSignup from './Components/InstructorSignup/InsSignup';
+import Welcomehome from './Components/InstructorSignup/welcomehome';
 
 function App() {
   return (
 <CssBaseline>
   <Header/>
+  {/* <Cart/> */}
   {/* <Signup /> */}
   {/* <Login /> */}
-  <InsSignup />
+  {/* <InsSignup /> */}
+  {/* <Welcomehome /> */}
   <Footer/>
 
 </CssBaseline>
