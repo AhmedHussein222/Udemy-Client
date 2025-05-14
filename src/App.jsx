8359959bee4de7e37aec09e7c051e41f0d8091ce
Ahmed Hussein <ahmdhussein222@gmail.com>
2025-05-14 12:45:31 +0300

    add course context after merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, } from "@mui/material";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Cart from "./Components/Cart/Cart";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import CreateCourse from "./Components/Instructor Dashboard/components/CreateCourse/createcourse";
-import InsMain from "./Components/Instructor Dashboard/components/Main/Main";
 import InsSignup from "./Components/Instructor Dashboard/InsSignup";
+import Welcomehome from "./Components/Instructor Dashboard/welcomehome";
 import Login from "./Components/LoginUsers/Login";
 import Signup from "./Components/SignUpStudents/Signup";
 import InsMain from "./Components/Instructor Dashboard/components/Main/Main";
@@ -24,6 +24,7 @@ import { UserContext } from "./context/UserContext";
 
 import InsHome from "./Components/Instructor Dashboard/components/Home/home";
 import EditCourse from "./Components/Instructor Dashboard/components/Edit Course/edit";
+import { CourseProvider } from './context/CourseContext';
 
 const router = createBrowserRouter([
   {
@@ -35,7 +36,6 @@ const router = createBrowserRouter([
       { path: "cart", element: <Cart /> },
       { path: "signup", element: <Signup /> },
       { path: "instructor-signup", element: <InsSignup /> },
-      { path: "", element: <Home /> },
     ],
   },
   { path: "instructor", element: <InsMain />, 
@@ -46,18 +46,19 @@ const router = createBrowserRouter([
       { path: "performance", element: <InsHome /> },
       { path: "create", element: <CreateCourse /> },
       { path: "edit", element: <EditCourse /> },
-    ],
+
+    ]
   },
 ]);
 
 function Main() {
   return (
     <>
-      <CssBaseline>
-        <Header />
-        <Outlet />
-        <Footer />
-      </CssBaseline>
+    <CssBaseline>
+      <Header />
+      <Outlet />
+      <Footer />
+    </CssBaseline>
     </>
   );
 }
@@ -102,7 +103,11 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <UserContext.Provider value={{ user }}>
+          <CourseProvider>
+
           <RouterProvider router={router} />
+          
+          </CourseProvider>
         </UserContext.Provider>
       </ThemeProvider>
     </CacheProvider>
@@ -110,3 +115,45 @@ const App = () => {
 };
 
 export default App;
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Layout from './Layout';
+// import Home from './Components/Home/Home';
+// import Cart from './Components/Cart/Cart';
+// import Signup from './Components/SignUpStudents/Signup';
+// import Login from './Components/LoginUsers/Login';
+// import InsSignup from './Components/InstructorSignup/InsSignup';
+// import Career from './HomeComponents/Career/Career'
+// // import Welcomehome from './Components/InstructorSignup/welcomehome';
+// import Sidebar from './Components/InstructorSignup/components/Drawer/sidebar';
+// import CreateCourse from './Components/InstructorSignup/components/CreateCourse/createcourse';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Layout />}>
+//           <Route index element={<Home />} />
+//           {/* <Route index element={<Welcomehome />} />  */}
+//           <Route path="cart" element={<Cart />} />
+//           <Route path="login" element={<Login />} />
+//           <Route path="signup" element={<Signup />} />
+//           <Route path="instructor-signup" element={<InsSignup />} />
+//           <Route path="sidebar" element={<Sidebar />} />
+//           <Route path="create-course" element={<CreateCourse />} />
+//           <Route path="career-accelerators" element={<Career />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
